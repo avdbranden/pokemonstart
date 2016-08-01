1645ccf583dc0e88ca49833ee1b078add15d89bf
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-08-01 17:58:01 +0200

    initialiaze the show

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,7 +1,9 @@
-class RestaurantsController < ApplicationController
+class PokemonsController < ApplicationController
   def index         # GET /pokemons
+    @pokemons = Pokemon.all
   end
 
   def show          # GET /pokemons/:id
+    @pokemon = Pokemon.find(params[:id])
   end
 end
