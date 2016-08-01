d8a333214c3b8733a1a568d69779c3bdbb034d58
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-08-01 15:42:49 +0200

    enrich models with best practice structure

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,6 +1,23 @@
 class Pokemon < ActiveRecord::Base
+  # == Constants ============================================================
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+
+  # == Relationships ========================================================
   has_many :moves, dependent: :destroy
   has_many :ratings, dependent: :destroy
+
+  # == Validations ==========================================================
   validates :name, presence: true, uniqueness: true
   validates :number, presence: true, uniqueness: true
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,28 @@
 class User < ActiveRecord::Base
+  # == Constants ============================================================
+
+  # == Attributes ===========================================================
+
+  # == Extensions ===========================================================
+
+  # == Relationships ========================================================
+  has_many :ratings, dependent: :destroy
+
+  # == Validations ==========================================================
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  # == Scopes ===============================================================
+
+  # == Callbacks ============================================================
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :ratings, dependent: :destroy
-  validates :email, presence: true, uniqueness: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+
+  # == Class Methods ========================================================
+
+  # == Instance Methods =====================================================
+
 end
