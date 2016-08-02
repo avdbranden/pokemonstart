df0aa36f08f24662e6816d96fd11e4790eb71d0d
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-08-02 14:50:32 +0200

    fixing bug with update rating - use of hidden_fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  # configure_permitted_parameters filter required to post simple_form with
+  # additional date through devise --> see https://github.com/plataformatec/devise
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,8 +1,8 @@
 class RatingsController < ApplicationController
-  def create         # POST /ratings
+  before_action :set_pokemon, only: %i(create update)
+
+  def create         # POST pokemons/:id/ratings
     @rating = Rating.new(rating_params)
-    @pokemon = Pokemon.find(params[:pokemon_id])
-    @rating.pokemon = @pokemon
     if @rating.save
       redirect_to pokemon_path(@pokemon)
     else
@@ -12,7 +12,6 @@ class RatingsController < ApplicationController
   end
 
   def update         # PATCH pokemons/:id/ratings/:id
-    @pokemon = Pokemon.find(params[:pokemon_id])
     @rating = Rating.find(params[:id])
     @rating.update(rating_params)
     @rating.save
@@ -21,7 +20,11 @@ class RatingsController < ApplicationController
 
   private
 
+  def set_pokemon
+    @pokemon = Pokemon.find(params[:pokemon_id])
+  end
+
   def rating_params
-    params.require(:rating).permit(:stars, :pokemon_id)
+    params.require(:rating).permit(:stars, :pokemon_id, :user_id)
   end
 end
