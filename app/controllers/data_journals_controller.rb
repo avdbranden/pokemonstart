3ea0d6c6f6afaa6f580f628b8bd18710b30fc458
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-08-03 10:41:26 +0200

    filter attributes to retain only personal data filled out by user

--- app/controllers/data_journals_controller.rb
+++ app/controllers/data_journals_controller.rb
@@ -4,6 +4,8 @@ class DataJournalsController < ApplicationController
   def index
     # If a data_journal is in DB, show it, otherwise, create one
     @data_journal.exists? ? @data_journal : create
+    @attributes = filter_attributes
+    # raise
   end
 
   def create
@@ -20,5 +22,17 @@ class DataJournalsController < ApplicationController
   def set_data_journal
     # Check if a data_journal exists for a given user_id
     @data_journal = DataJournal.where(user_id: params[:user_id])
+  end
+
+  def filter_attributes
+    # Remove undesired elements from user model
+    # Retain personal data
+    current_user.attributes.slice!(
+      "id", "encrypted_password", "reset_password_token",
+      "reset_password_sent_at","remember_created_at", "sign_in_count",
+      "current_sign_in_at", "last_sign_in_at", "current_sign_in_ip",
+      "last_sign_in_ip", "created_at","updated_at", "admin"
+    # Remove personal data not filled out by user
+      ).delete_if { |type, data| data.blank? }
   end
 end
