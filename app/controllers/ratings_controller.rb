f1b95f1e525314f053aa250f053906b0231babf2
Adrien van den Branden de Reeth <avdbranden@gmail.com>
2016-08-03 12:01:34 +0200

    solve issue with ratings showing and not showing

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,12 +2,11 @@ class RatingsController < ApplicationController
   before_action :set_pokemon, only: %i(create update)
 
   def create         # POST pokemons/:id/ratings
-    @rating = Rating.new(rating_params)
+    @rating = @pokemon.ratings.build(rating_params)
     if @rating.save
       redirect_to pokemon_path(@pokemon)
     else
       render "pokemons/show"
-      @rating.errors.full_messages
     end
   end
 
